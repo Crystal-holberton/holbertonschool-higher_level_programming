ec4412a6bed49f7836aadcaedc7772ac5a8e88d7
Crystal-holberton <9188@holbertonstudents.com>
2025-01-23 15:29:59 +1100

    check spelling

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -19,9 +19,9 @@ def matrix_divided(matrix, div):
         List: a new matrix
     """
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of intergers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not all(isinstance(num, (int, float)) for row in matrix for num in row):
-        raise TypeError("matrix must be a matrix (list of lists) of intergers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     row_lengths = [len(row) for row in matrix]
     if len(set(row_lengths)) != 1:
         raise TypeError("Each row of the matrix must have the same size")
