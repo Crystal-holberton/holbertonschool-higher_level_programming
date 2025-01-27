144c2d8325ca1f0db7ed962aa9341cce998c2c42
Crystal-holberton <9188@holbertonstudents.com>
2025-01-28 10:32:28 +1100

    square size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+"""A class that defines a square"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        """Defines a square based on 0-square.py"""
+        self.__size = size
