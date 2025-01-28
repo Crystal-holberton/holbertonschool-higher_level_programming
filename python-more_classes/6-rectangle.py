ef6725ff6d19cb9b4eb7100759290cb75ccf93af
Crystal-holberton <9188@holbertonstudents.com>
2025-01-29 10:56:57 +1100

    add attribute

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -3,7 +3,13 @@
 
 
 class Rectangle:
-    """Represents a rectangle"""
+    """
+    Represents a rectangle
+    Attributes:
+    number_of_instances (int): number of rectangle instances
+    """
+
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """
