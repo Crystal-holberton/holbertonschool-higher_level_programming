e877956dd88003c005af94a8690b4d684966499a
Crystal-holberton <9188@holbertonstudents.com>
2025-02-03 15:37:32 +1100

    base geometry errors

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Create empty class 'BaseGeometry'"""
+
+
+class BaseGeometry:
+    """Represents base geometry"""
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates an integer
+
+        Args:
+        name (str): name of parameter
+        value (int): parameter to validate
+        Raises:
+        TypeError: if value is not an integer
+        ValueError: if value is <= 0
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{}must be greater than 0".format(name))
