9584cbefb23eeee994bd688e31fa384455855608
Crystal-holberton <9188@holbertonstudents.com>
2025-02-19 14:03:10 +1100

    requests

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Python script to fetch posts from JSONPlaceholder"""
+import requests
+
+# Define URL
+url = 'https://jsonplaceholder.typicode.com/posts'
+
+
+def fetch_and_print_posts():
+    """Send request to JSONPlaceholder"""
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        data = response.json()
+        for resp in data:
+            title = resp["title"]
+            print(title)
+    else:
+        print("Request was not successful.")
+
+
+def fetch_and_save_posts():
+    """Save response data to a dictionary"""
+    import csv
+
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        data = response.json()
+        csvf = 'posts.csv'
+        datalist = []
+        for element in data:
+            post = {
+                'id': element.get('id'),
+                'title': element.get('title'),
+                'body': element.get('body')
+            }
+            datalist.append(post)
+        with open(csvf, 'w', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(datalist)
+    else:
+        print("request was not successful.")
