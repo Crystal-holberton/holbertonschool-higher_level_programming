6c7be13df88f4fb32db9464213adb3d66e31ab8f
Crystal-holberton <9188@holbertonstudents.com>
2025-03-07 14:26:30 +1100

    added blank line

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # Create the Base instance
 Base = declarative_base()
 
+
 class State(Base):
     """State class that maps to the states table in the database"""
     __tablename__ = 'states'
