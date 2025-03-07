f8dc3a935236ff50f1206cc37bbcc8acb56da5b2
Crystal-holberton <9188@holbertonstudents.com>
2025-03-07 12:36:42 +1100

    select states and tasks 0-4

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get MySQL credentials from command-line arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    # Connect to MySQL database
+    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+    cursor = db.cursor()
+    # Execute query to select all states ordered by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # Fetch all results and print them
+    for state in cursor.fetchall():
+        print(state)
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
